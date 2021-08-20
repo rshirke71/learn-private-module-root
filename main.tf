f143c3db8d126e948627940ca551ad226cddcaaf
rshirke71 <48396127+rshirke71@users.noreply.github.com>
2021-08-20 17:00:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "s3-webapp" {
   source  = "app.terraform.io/aims/s3-webapp/aws"
-  name        = var.name
+  name   = var.name
   region = var.region
   prefix = var.prefix
   version = "1.0.0"
 }
+
